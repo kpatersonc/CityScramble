174d24fcc7592f8645ed7e551d977946f21e9566
CORP\mpokrajac <mpokrajac@infusion.com>
2017-06-23 14:35:13 -0400

    Initial LeaderBoard changes

--- InfusionGames.CityScramble/InfusionGames.CityScramble/ViewModels/LeaderViewModel.cs
+++ InfusionGames.CityScramble/InfusionGames.CityScramble/ViewModels/LeaderViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Windows.Input;
 using Caliburn.Micro;
 using InfusionGames.CityScramble.Models;
 using InfusionGames.CityScramble.Services;
 using Xamarin.Forms;
+using Caliburn.Micro.Xamarin.Forms;
 
 namespace InfusionGames.CityScramble.ViewModels
 {
@@ -16,6 +18,35 @@ namespace InfusionGames.CityScramble.ViewModels
         /// <summary>
         /// Navigation Parameter
         /// </summary>
+        /// 
+        private readonly IDataService _dataService;
+        private readonly INavigationService _navService;
+        private readonly ISettingsService _setService;
+
+        public LeaderViewModel(IDataService dataService, INavigationService navService, ISettingsService setService)
+        {
+            _dataService = dataService;
+            _navService = navService;
+            _setService = setService;
+            Teams = new BindableCollection<Team>();
+           
+        }
+
+
+        private string _ourTeamName;
+        public string OurTeamName
+        {
+            get { return _ourTeamName; }
+            set { SetField(ref _ourTeamName, value); }
+        }
+
+        private int _ourTeamPoints;
+        public int OurTeamPoints
+        {
+            get { return _ourTeamPoints; }
+            set { SetField(ref _ourTeamPoints, value); }
+        }
+
         public Race SelectedRace { get; set; }
 
         public string Title { get; private set; } = "Leader Board";
@@ -24,10 +55,48 @@ namespace InfusionGames.CityScramble.ViewModels
 
         public int Priority => 1;
 
+        
+
+    private BindableCollection<Team> _teams;
+        public BindableCollection<Team> Teams { get; protected set; }
+
         public bool IsSupported(Race race)
         {
             return true;
-        } 
+        }
+
+        protected override async void OnActivate()
+        {
+            var race = await _dataService.GetRaceAsync(SelectedRace.Id);
+            //base.OnActivate();
+            IsBusy = true;
+            var sortedTeams = race.Teams.OrderByDescending(t => t.Points).Take(3);
+            foreach (var team in sortedTeams)
+            {
+                Teams.Add(team);
+                if(team.Name == _setService.MyTeamName)
+                {
+                    OurTeamName = team.Name;
+                    OurTeamPoints = team.Points;
+                }
+            }
+            IsBusy = false;
+        }
+
+        protected override async void OnInitialize()
+        {
+            //var test = SelectedRace.Teams;
+            //var race = await _dataService.GetRaceAsync(SelectedRace.Id);
+            //base.OnInitialize();
+            //OnActivate();
+            //IsBusy = true;
+
+            //foreach (var team in race.Teams)
+            //{
+            //    Teams.Add(team);
+            //}
+            //IsBusy = false;
+        }
         #endregion
     }    
 }
